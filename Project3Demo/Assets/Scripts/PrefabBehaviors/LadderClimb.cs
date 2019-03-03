7e5971338f67623a5cdad1fce273c1d3b9f8c809
niklaus.bair <niklaus.bair@mymail.champlain.edu>
2019-03-03 11:54:26 -0500

    Updated ladder prefab and script

--- Project3Demo/Assets/Scripts/PrefabBehaviors/LadderClimb.cs
+++ Project3Demo/Assets/Scripts/PrefabBehaviors/LadderClimb.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
+using Rewired;
 
 public class LadderClimb : MonoBehaviour
 {
     [SerializeField] private float upwardRate;
 
+    Player rewiredPlayer;
+
+    private void Start()
+    {
+        rewiredPlayer = ReInput.players.GetPlayer("Diver");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Diver")
         {
-            gameObject.GetComponent<PlayerDiverMovement>().isGrounded = false;
+            other.gameObject.GetComponent<PlayerDiverMovement>().isGrounded = false;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Diver")
+        if (other.gameObject.tag == "Diver" && rewiredPlayer.GetButton("Interact"))
         {
             other.gameObject.GetComponent<CharacterController>().Move(Vector3.up * upwardRate);
         }
