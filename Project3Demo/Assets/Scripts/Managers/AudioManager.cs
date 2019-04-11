f2de0b39c6434380305f891ae5e685f6202b9000
niklaus.bair <niklaus.bair@localhost>
2019-04-10 20:25:23 -0400

    Updated AudioManager to avoid build issues

--- Project3Demo/Assets/Scripts/Managers/AudioManager.cs
+++ Project3Demo/Assets/Scripts/Managers/AudioManager.cs
@@ -1,13 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
 public class AudioManager : MonoBehaviour
 {
     [HideInInspector] public static AudioManager instance;
 
-    [SerializeField] private string data = "Assets/Resources/Audio/audio_data.txt";
+    private readonly string[] data =
+    {
+        "Motor1, electric_motor",
+        "Motor2, electric_motor2",
+        "Footsteps, footsteps",
+        "Ambience1, 193822__jhumbucker__deep-sea-ambience",
+        "Ambience2, underwater_ambience",
+        "Ambience3, underwater_ambience2",
+        "Soundscape1, soundscape",
+        "SlowBreathing1, slow_breathing",
+        "SlowBreathing2, 324562__univ-lyon3__saly-margaux-2015-2016-underwater-breathing",
+        "WaterMotion, 262754__klankbeeld__water-sand-moving-003-140811-0365",
+        "WaveSounds, 267245__klankbeeld__wave-hydrophone-001",
+        "MetalHit1, close_metal_hit",
+        "MetalHit2, distant_metal_hit",
+        "MetalPush, 352745__rosebugg__fence",
+        "MetalCrash, metal_crashing",
+        "MetalDoorClose, metal_door_close",
+        "MetalDoorOpen, metal_door_open",
+        "ImpactE, underwater_impact",
+        "Impact, unmodified_impact",
+        "SinkingCrash, 232021__ubehag__stereotypical-sinking-submarine-sound-no-reverb",
+        "HullCreak, 412766__ohnobones__underwater-hullcreaking",
+        "Flare, flare",
+        "BurningFlare, burning_flare",
+        "FlareAmmoBoxPickup, 399122__fastson__magazinerelease-01",
+        "EmptyFlareGunClick, 170272__knova__gun-click",
+        "Monster1, monster_1",
+        "Monster1e, monster_1_e",
+        "Monster2, monster_2",
+        "Monster2e, monster_2_e",
+        "Monster3, monster_3",
+        "Monster3e, monster_3_e",
+        "Monster4, monster_4",
+        "Monster4e, monster_4_e",
+        "Monster5, monster_5",
+        "Monster5e, monster_5_e",
+        "Monster6, monster_6",
+        "Monster6e, monster_6_e",
+        "Monster7, monster_7",
+        "Monster7e, monster_7_e",
+        "Monster8, monster_8",
+        "Monster8e, monster_8_e",
+        "Monster9, monster_9",
+        "Monster9e, monster_9_e",
+        "Monster10, monster_10",
+        "Monster10e, monster_10_e",
+        "SeaMonster1, 151852__carroll27__deep-sea-creature-noise-ambient-sound-of-the-deep",
+        "SeaMonster2, 395476__chilljeremy__sea-monster",
+        "Groan, 260637__rcarpo__deep-sea-groan"
+    };
 
     private Dictionary<string, AudioClip> clips;
 
@@ -23,16 +72,12 @@ public class AudioManager : MonoBehaviour
 
         clips = new Dictionary<string, AudioClip>();
 
-        StreamReader read = new StreamReader(data);
-
         // Read in audio queue data line by line in the format:
         //  ClipName, clip_file_location
-
-        while (!read.EndOfStream)
+        foreach (string path in data)
         {
-            string line = read.ReadLine();
-            int split = line.IndexOf(','); ;
-            clips[line.Substring(0, split)] = Resources.Load<AudioClip>("Audio/" + line.Substring(split + 2));              
+            int split = path.IndexOf(','); ;
+            clips[path.Substring(0, split)] = Resources.Load<AudioClip>("Audio/" + path.Substring(split + 2));              
         }
 
         if (debuggingMode)
