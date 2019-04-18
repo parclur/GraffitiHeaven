b1dbf59f3a6febd4c39c88cf78fd447f0ab2a20c
andrew.macDonald <andrew.macDonald@localhost>
2019-04-18 19:16:12 -0400

    Colapse script

---
+++ Project3Demo/Assets/CollapseEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapseEvent : MonoBehaviour, ITriggerable
+{
+    [SerializeField] private GameObject floor;
+    [SerializeField] private float collapseTime;
+
+    public void TriggerEvent()
+    {
+        StartCoroutine(Collapse());
+    }
+
+    private IEnumerator Collapse()
+    {
+        float time = 0f;
+
+        Vector3 startPos = floor.transform.position;
+        Vector3 endPos = startPos + (Vector3.down * 10f);
+
+        while (time < collapseTime)
+        {
+            floor.transform.position = Vector3.Lerp(startPos, endPos, (time / collapseTime));
+            time += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
