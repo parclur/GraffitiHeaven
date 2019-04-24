2ae695717287cff905a23dba4002946058b23234
austin.laimos <austin.laimos@localhost>
2019-04-24 12:07:46 -0400

    Added a way to decrement the actual battery.

---
+++ Project3Demo/Assets/Scripts/PrefabBehaviors/DecrementBattery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecrementBattery : MonoBehaviour {
+    
+    [SerializeField] int batterySlots;
+
+    void OnTriggerEnter(Collider col){
+        UpdateActualBattery.instance.DecrementBattery(batterySlots);
+    }
+}
