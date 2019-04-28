42c558a796c785c93d4115d345b063c357374b85
austin.laimos <austin.laimos@localhost>
2019-04-27 23:01:40 -0400

    Added a back to title to the game. It needs to be a titlewithoutrewired because then there would be two rewireds and thats bad.

---
+++ Project3Demo/Assets/Scripts/Controls/MenuControls/BackToTitle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToTitle : MenuItemCall {
+
+    void Start(){
+        nextMenu = false;
+    }
+
+    public override void ClickCall(){
+        SceneManager.LoadScene("TitleWithoutRewired");
+    }
+
+    public override void OnHighlight(float x) { }
+}
