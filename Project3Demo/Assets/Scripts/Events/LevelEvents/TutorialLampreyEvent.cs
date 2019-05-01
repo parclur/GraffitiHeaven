6115f4eae5a3b5caca17455523035037d40ae0b6
cyeash <cyeash@localhost>
2019-04-30 21:36:56 -0400

    Adding meta data for the eggs. Hopefully that will fix it

---
+++ Project3Demo/Assets/Scripts/Events/LevelEvents/TutorialLampreyEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLampreyEvent : MonoBehaviour
+{
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // When the player enters the triggerzone, the door starts to open
+    public void TriggerEvent()
+    {
+        if (!door.GetComponent<HeavyDoor>().doorOpen)
+        {
+            // Open door
+            door.GetComponent<HeavyDoor>().Activate();
+        }
+    }
+}
